b145d9a1156946a4d8e12c23d14dfa7302ce32c8
Mikhail-net-ne-mikhail <72822216+Mikhail-net-ne-mikhail@users.noreply.github.com>
2020-12-09 11:31:27 +0300

    Laba 7 variant 13

---
+++ LABA7VAR13.cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+void prikol(char* str)
+{
+    char* src = str, * dst = str;
+    int space = 1;
+    while (*src) {
+        if (space && *src == ' ') {
+            ++src;
+            continue;
+        }
+        if (space)
+            space = 0;
+        if (*src == ' ')
+            space = 1;
+        *dst++ = *src++;
+    }
+    if (*dst == ' ')
+        --dst;
+    *dst = '\0';
+}
+
+
+int main()
+{
+    setlocale(0, "Russian");
+    char str[100];
+    printf("Введите строку\n");
+    gets_s(str);
+    prikol(str);
+    printf("Строка после изменения\n");
+    puts(str);
+    return 0;
+}
