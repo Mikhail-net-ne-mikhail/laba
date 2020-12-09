a087605319fcd39a43fdac6a72c5d05d56d72de9
Mikhail-net-ne-mikhail <72822216+Mikhail-net-ne-mikhail@users.noreply.github.com>
2020-12-09 11:37:18 +0300

    Laba 3

---
+++ laba3varik8.cpp
@@ -0,0 +1,41 @@
+﻿#include <stdio.h> 
+#include<clocale>
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int k, n, m;
+	int i = 0;
+
+	printf("Введите число n\n");
+	scanf_s("%d", &n);
+	printf("Введите количество цилов k\n");
+	scanf_s("%d", &k);
+
+
+
+	do
+	{
+		if (n % 2 == 0)
+		{
+			n = n / 2;
+			i++;
+		}
+		else
+		{
+			n = n * 3 + 1;
+			i++;
+		}
+	} while (n > 1);
+	if (k < i)
+	{
+
+		printf("i=%d\n", i);
+		printf("n=%d\n", n);
+	}
+	else
+	{
+		printf("Условие 1<k<n не выполнено\n");
+	}
+
+	return 0;   
+}
